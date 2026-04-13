fb974aac6ca30c226d41608fdccbf71e1c066b19
ilywka_ <illasalnikov6@gmail.com>
2026-04-13 13:55:02 +0300

    add/model-connected-to-index.html

--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    all_products = Product.objects.all()
+
+    context = {
+        'products': all_products
+    }
+
+    return render(request, 'index.html', context)
 
 def catalog_page(request):
     return render(request, 'catalog.html')
