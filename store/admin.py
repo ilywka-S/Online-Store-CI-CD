5104dca530cd36e4a489dda99bb0e47a8af45d90
ilywka_ <illasalnikov6@gmail.com>
2026-04-16 12:59:44 +0300

    registered-models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price']
+    list_filter = ['category']
+    search_fields = ['name', 'category']
+    list_editable = ['price']
